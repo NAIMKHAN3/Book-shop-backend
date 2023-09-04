c726eeec47c4e0c4a077df12e4e394c41b9977c4
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-09-04 18:35:21 +0600

    book interface in model done

---
+++ src/module/book/book.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import { IBook } from "./book.interface";
+
+const bookSchema = new Schema<IBook>({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    publicationDate: {
+        type: Date,
+        required: true,
+        default: new Date()
+    },
+},
+{timestamps: true})
+
+export const Book = model<IBook>('Book', bookSchema)
