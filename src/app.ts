acbe3988afc33d6669f04445cc76805b11eacc19
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-09-04 14:09:49 +0600

    global error handle done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import errorHandler, { notfoundandler } from './middleware/errorHandler'
+import handleValidationError from './middleware/handleMongooseValidationError'
 
 const app: Application = express()
 
@@ -11,5 +13,8 @@ app.get('/', (req: Request, res: Response) => {
     .status(200)
     .send({ success: true, message: 'University Management Server is Running' })
 })
+app.use(errorHandler)
+app.use(notfoundandler)
+app.use(handleValidationError)
 
 export default app
